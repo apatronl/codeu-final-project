2db01e7a80b75c7a55f993e1b88500875fa6219e
apatronl <apl7@gatech.edu>
2016-08-02 18:10:47 -0700

    Added filter

--- src/com/flatironschool/javacs/Cli.java
+++ src/com/flatironschool/javacs/Cli.java
@@ -3,11 +3,11 @@ package com.flatironschool.javacs;
 import com.lexicalscope.jewel.cli.Option;
 
 public interface Cli {
-	
+
 	/**
 	* Gets a single term to search
-	*/	
+	*/
 	@Option
 	String getTerm();
+	String getFilter();
 }
-
